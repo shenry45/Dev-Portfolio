5096eea6d88b97cafcd5cb21946ae89349183966
shenry45 <shawn45henry@gmail.com>
2020-08-05 20:45:41 -0700

    build dir contents

---
+++ build/precache-manifest.cacb2ea2dac825c472745321adbd99a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47abe465b52d46c668c0a1ae86bcf030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7c80062039bc3cd3c9e",
+    "url": "/static/css/main.b8561c83.chunk.css"
+  },
+  {
+    "revision": "0e1f68769fd920c908ca",
+    "url": "/static/js/2.3c1cc40c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3c1cc40c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c80062039bc3cd3c9e",
+    "url": "/static/js/main.c7f14174.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "9849e15ea586f8967fad1c021082dbd8",
+    "url": "/static/media/about-shawn-henry-portrait.9849e15e.jpg"
+  },
+  {
+    "revision": "a3911117af01e56b7050d57517ba42f9",
+    "url": "/static/media/concert-searcher-show-image.a3911117.jpg"
+  },
+  {
+    "revision": "03c5c94aca3781aaa17f93b4daf16ce6",
+    "url": "/static/media/full-stack-developer-text.03c5c94a.svg"
+  },
+  {
+    "revision": "c26f425a996e47676445cd66ec4a76f4",
+    "url": "/static/media/how-to-database-show-image.c26f425a.jpg"
+  },
+  {
+    "revision": "21ffce0fffb4bfb9cb32a888591882c9",
+    "url": "/static/media/neighborhood-library-show-image.21ffce0f.jpg"
+  }
+]);
